97bb616f700c73572050c86a13aefe63a2d1869a
yaozeyuan001 <yaozeyuan001@ke.com>
2021-04-14 16:36:26 +0800

    feat: 支持设定周报时间

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const path = require("path");
 const argv = require("minimist")(process.argv.slice(2));
 
 const baseWorkDirPath = path.resolve(".");
-const YearAt = argv["year"] || moment().year();
+let YearAt = argv["year"] || moment().year();
+YearAt = Number.parseInt(YearAt);
+if (Number.isInteger(YearAt) === false || YearAt < 2000 || YearAt > 3000) {
+  // 重置为默认值
+  YearAt = moment().year();
+}
+
+let weekReportAt = argv["report_at"] || 5;
+weekReportAt = Number.parseInt(weekReportAt);
+if (
+  Number.isInteger(weekReportAt) === false ||
+  weekReportAt < 1 ||
+  weekReportAt > 7
+) {
+  // 重置为默认值
+  weekReportAt = 5;
+}
+
+console.log(`创建${YearAt}年的周报列表. 每周提交周报时间为: 周${weekReportAt}`);
 
 for (let monthAt = 1; monthAt <= 12; monthAt++) {
   let strMonthAt = (monthAt + "").padStart(2, 0);
@@ -21,7 +39,7 @@ for (let monthAt = 1; monthAt <= 12; monthAt++) {
   ) {
     let strDayIndex = `${dayIndex}`.padStart(2, "0");
     let k = moment(`${YearAt}-${strMonthAt}-${strDayIndex}`).weekday();
-    if (k === 4) {
+    if (k === weekReportAt) {
       let initAtStr = `${YearAt}-${strMonthAt}-${strDayIndex} 00:00:00`;
       //   console.log('initAtStr => ', initAtStr)
       let endDayAt = moment(initAtStr, "YYYY-MM-DD HH:mm:ss")
@@ -33,10 +51,10 @@ for (let monthAt = 1; monthAt <= 12; monthAt++) {
       shell.mkdir("-p", targetFloderPath);
       let targetFileUri = `${baseWorkDirPath}/工作报告${YearAt}/${strMonthAt}月/${strDayIndex}-${strEndDayAt}.md`;
       targetFileUri = path.resolve(targetFileUri);
-      console.log("targetFloderPath => ", targetFloderPath);
-      console.log("targetFileUri => ", targetFileUri);
+      //   console.log("targetFloderPath => ", targetFloderPath);
+      //   console.log("targetFileUri => ", targetFileUri);
       shell.touch(targetFileUri);
-      console.log(targetFileUri);
+      console.log("创建周报文件:", targetFileUri);
     }
   }
 }
